2a4beaa69b394f82737162a414b76742654347b1
Achutha Mullapudi <achuthamullapudi@mac.mynetworksettings.com>
2025-03-12 20:20:59 -0400

    added and imported basic config packages

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+import os
+import pkgutil
+import importlib
+import sys
+from app.commands import CommandHandler, Command
+from dotenv import load_dotenv
+import logging
+import logging.config
+
+class App:
+    def __init__(self):
+        os.makedirs('logs', exist_ok=True)
+        self.configure_logging()
+        load_dotenv()
+        self.settings = self.load_environment_variables()
+        self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
+        self.command_handler = CommandHandler()
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
