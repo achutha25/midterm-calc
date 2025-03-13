21aacb2cd52f8b9ffa09ac198ab47a8d54cad99c
Achutha Mullapudi <achuthamullapudi@mac.mynetworksettings.com>
2025-03-13 15:14:30 -0400

    fixing indentation errors and installing dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ class App:
         self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
         self.command_handler = CommandHandler()
 
- def configure_logging(self):
+    def configure_logging(self):
         logging_conf_path = 'logging.conf'
         if os.path.exists(logging_conf_path):
             logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
@@ -24,15 +24,15 @@ class App:
             logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
         logging.info("Logging configured.")
 
-def load_environment_variables(self):
+    def load_environment_variables(self):
         settings = {key: value for key, value in os.environ.items()}
         logging.info("Environment variables loaded.")
         return settings
 
-def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
+    def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
         return self.settings.get(env_var, None)
 
-def load_plugins(self):
+    def load_plugins(self):
         plugins_package = 'app.plugins'
         plugins_path = plugins_package.replace('.', '/')
         if not os.path.exists(plugins_path):
@@ -46,17 +46,16 @@ def load_plugins(self):
                 except ImportError as e:
                     logging.error(f"Error importing plugin {plugin_name}: {e}")
 
-def register_plugin_commands(self, plugin_module, plugin_name):
+    def register_plugin_commands(self, plugin_module, plugin_name):
         for item_name in dir(plugin_module):
             item = getattr(plugin_module, item_name)
             if isinstance(item, type) and issubclass(item, Command) and item is not Command:
                 self.command_handler.register_command(plugin_name, item())
                 logging.info(f"Command '{plugin_name}' from plugin '{plugin_name}' registered.")
 
-
- def start(self):
+    def start(self):
         self.load_plugins()
-        logging.info("Application started.please Type 'exit' to exit.")
+        logging.info("Application started. Please type 'exit' to exit.")
         try:
             while True:
                 cmd_input = input(">>> ").strip()
@@ -69,7 +68,7 @@ def register_plugin_commands(self, plugin_module, plugin_name):
                     logging.error(f"Unknown command: {cmd_input}")
                     sys.exit(1)
         except KeyboardInterrupt:
-            logging.info("Application interrupted and exiting .")
+            logging.info("Application interrupted and exiting.")
             sys.exit(0)
         finally:
             logging.info("Application shutdown.")
@@ -77,3 +76,4 @@ def register_plugin_commands(self, plugin_module, plugin_name):
 if __name__ == "__main__":
     app = App()
     app.start()
+
